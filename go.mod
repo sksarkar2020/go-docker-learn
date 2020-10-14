3105bc091497d7c2ca74c3dcacdab13bcd3c44b5
santosh k. sarkar <ssantosh.sarkar@gmail.com>
2020-10-14 17:16:34 +0530

    changes

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/sksarkar2020/go-docker-learn
+
+go 1.15
+
+require (
+	github.com/google/go-cmp v0.5.2 // indirect
+	github.com/gorilla/mux v1.8.0
+	rsc.io/quote v1.5.2
+)
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello, world!\n")
+}
+
+// Route declaration
+func router() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/", handler)
+	return r
+}
+
+// Initiate web server
+func main() {
+	router := router()
+	srv := &http.Server{
+		Handler:      router,
+		Addr:         "127.0.0.1:9100",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
+}
